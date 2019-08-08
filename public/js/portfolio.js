d03b9412ffd83739feece5521200375b6fe7e7fe
Acácio S. Costa <acaciosantossi@gmail.com>
2019-08-08 20:02:57 -0300

    Teste Front-End

---
+++ public/js/portfolio.js
@@ -0,0 +1,143 @@
+function allCards() {
+    document.getElementById('telaDinamica').innerHTML = `<div class="card-deck text-white">
+                                                        <div class="card">
+                                                            <img class="card-img-top" src="./public/images/image-card.png">
+                                                            <div class="card-body bgcards">
+                                                                <h5 class="card-title">Design</h5>
+                                                                <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                                                            </div>
+                                                        </div>
+                                                        <div class="card">
+                                                            <img class="card-img-top" src="./public/images/image-card.png">
+                                                            <div class="card-body bgcards">
+                                                                <h5 class="card-title">Identify</h5>
+                                                                <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                                                            </div>
+                                                        </div>
+                                                        <div class="card">
+                                                            <img class="card-img-top" src="./public/images/image-card.png">
+                                                            <div class="card-body bgcards">
+                                                                <h5 class="card-title">Design</h5>
+                                                                <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                    <div class="card-deck mt-5 text-white">
+                                                        <div class="card">
+                                                            <img class="card-img-top" src="./public/images/image-card.png">
+                                                            <div class="card-body bgcards">
+                                                                <h5 class="card-title">Photography</h5>
+                                                                <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                                                            </div>
+                                                        </div>
+                                                        <div class="card bgcards">
+                                                            <img class="card-img-top" src="./public/images/img-link-portfolio.png">
+                                                        </div>
+                                                        <div class="card">
+                                                            <img class="card-img-top" src="./public/images/image-card.png">
+                                                            <div class="card-body bgcards">
+                                                                <h5 class="card-title">Identify</h5>
+                                                                <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                    <div class="card-deck mt-5 text-white mb-5">
+                                                        <div class="card">
+                                                            <img class="card-img-top" src="./public/images/image-card.png">
+                                                            <div class="card-body bgcards">
+                                                                <h5 class="card-title">Ilustration</h5>
+                                                                <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                                                            </div>
+                                                        </div>
+                                                        <div class="card">
+                                                            <img class="card-img-top" src="./public/images/image-card.png">
+                                                            <div class="card-body bgcards">
+                                                                <h5 class="card-title">Ilustration</h5>
+                                                                <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                                                            </div>
+                                                        </div>
+                                                        <div class="card">
+                                                            <img class="card-img-top" src="./public/images/image-card.png">
+                                                            <div class="card-body bgcards">
+                                                                <h5 class="card-title">Design</h5>
+                                                                <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                                                            </div>
+                                                        </div>
+                                                    </div>`
+}
+
+function design() {
+    document.getElementById('telaDinamica').innerHTML = `<div class="card-deck text-white">
+                                                        <div class="card">
+                                                            <img class="card-img-top" src="./public/images/image-card.png">
+                                                            <div class="card-body bgcards">
+                                                                <h5 class="card-title">Design</h5>
+                                                                <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                                                            </div>
+                                                        </div>
+                                                        <div class="card">
+                                                            <img class="card-img-top" src="./public/images/image-card.png">
+                                                            <div class="card-body bgcards">
+                                                                <h5 class="card-title">Design</h5>
+                                                                <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                                                            </div>
+                                                        </div>
+                                                        <div class="card">
+                                                            <img class="card-img-top" src="./public/images/image-card.png">
+                                                            <div class="card-body bgcards">
+                                                                <h5 class="card-title">Design</h5>
+                                                                <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                                                            </div>
+                                                        </div>
+                                                    </div>`
+}
+
+function identity() {
+    document.getElementById('telaDinamica').innerHTML = `<div class="card-deck text-white">
+                                                            <div class="card">
+                                                            <img class="card-img-top" src="./public/images/image-card.png">
+                                                            <div class="card-body bgcards">
+                                                                <h5 class="card-title">Identify</h5>
+                                                                <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                                                            </div>
+                                                        </div>
+                                                        <div class="card">
+                                                            <img class="card-img-top" src="./public/images/image-card.png">
+                                                            <div class="card-body bgcards">
+                                                                <h5 class="card-title">Identify</h5>
+                                                                <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                                                            </div>
+                                                        </div>
+                                                    </div>`
+}
+
+function photography() {
+    document.getElementById('telaDinamica').innerHTML = `<div class="card-deck text-white">
+                                                            <div class="card">
+                                                                <img class="card-img-top" src="./public/images/image-card.png">
+                                                                <div class="card-body bgcards">
+                                                                    <h5 class="card-title">Photography</h5>
+                                                                    <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                                                                </div>
+                                                            </div>
+                                                        </div>`
+}
+
+function ilustration() {
+    document.getElementById('telaDinamica').innerHTML = `<div class="card-deck text-white">
+                                                            <div class="card">
+                                                                <img class="card-img-top" src="./public/images/image-card.png">
+                                                                <div class="card-body bgcards">
+                                                                    <h5 class="card-title">Ilustration</h5>
+                                                                    <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                                                                </div>
+                                                            </div>
+                                                            <div class="card">
+                                                                <img class="card-img-top" src="./public/images/image-card.png">
+                                                                <div class="card-body bgcards">
+                                                                    <h5 class="card-title">Ilustration</h5>
+                                                                    <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                                                                </div>
+                                                            </div>
+                                                        </div>`
+}
