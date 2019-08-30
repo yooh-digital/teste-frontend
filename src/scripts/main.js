e51258ffab3d942ee4facdc13c6d989fd231ed34
Luan Fellipe <luan.fellipe@live.com>
2019-08-29 23:05:46 -0300

    seções do site

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,9 +1,12 @@
-// (function() {
-//     function menuMobile() {
-//         $(function(){
-//             $('#nave-bar').slicknav();
-//         });
-//     }
+(function() {
+    function menuMobile() {
+        $('#nav-bar ul').origamidMenu({
+            breakpoint: 1024,
+            top: 35,
+            background: '#2c3e50',
+            color: '#FFFFFF',
+        });
+    }
 
-//     menuMobile();
-// }());
+    menuMobile();
+}());
